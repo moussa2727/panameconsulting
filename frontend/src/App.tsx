cd1867ce8c23f168c5be3b431e634e70ac2c6a03
moussa-sangare <moussa.sangare.ma@gmail.com>
2025-11-14 01:11:44 +0000

    with cookie consent management removed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,6 @@ import 'aos/dist/aos.css';
 import { Helmet } from 'react-helmet-async';
 import ErrorBoundary from './components/ErrorBoundary';
 import { useAuth } from './utils/AuthContext';
-import CookieBanner from './components/CookieBanner';
 
 // Components communs
 import Header from './components/Header';
@@ -222,7 +221,6 @@ function App() {
           <Route path='*' element={<NotFound />} />
         </Routes>
       </div>
-      <CookieBanner />
     </ErrorBoundary>
   );
 }
