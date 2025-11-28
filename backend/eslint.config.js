ff16b4c76bc42bf3dc334b6ae81a7817cbf96329
moussa-sangare <moussa.sangare.ma@gmail.com>
2025-11-28 00:34:27 +0000

    corrected

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,14 +1,31 @@
 // eslint.config.js
 const js = require('@eslint/js');
 const tseslint = require('typescript-eslint');
+const globals = require('globals');
 
 module.exports = [
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
     files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
     rules: {
-      'no-undef': 'off',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': ['error', { 
         'argsIgnorePattern': '^_',
@@ -16,9 +33,15 @@ module.exports = [
       }],
       'prefer-const': 'error',
       'no-var': 'error',
-    }
+    },
   },
   {
-    ignores: ['dist/**', 'node_modules/**', '**/*.js']
+    ignores: [
+      'dist/**', 
+      'node_modules/**', 
+      '**/*.js',
+      'coverage/**',
+      '*.config.js'
+    ]
   }
 ];
